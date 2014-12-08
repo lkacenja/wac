3ceecf001a51cec36a37666d6993de2e7b215e32
lkacenja <lkacenja@gmail.com>
2014-12-08 23:42:19 +0000

    Allowing for Drupal 6 behaviors.

--- theme/js/chart-behavior.js
+++ theme/js/chart-behavior.js
@@ -1,21 +1,26 @@
 (function (Class, Drupal) {
-  Drupal.behaviors.Wac = {
-    attach: function() {
-      if (Drupal.settings.wac) {
-        var x, element, chart;
-        for (var x in Drupal.settings.wac) {
-            element = document.getElementById(Drupal.settings.wac[x].id);
-          if ((' ' + element.className + ' ').indexOf(' ' + 'wac-chart-processed' + ' ') < 0) {
-            chart = new Drupal.Wac[Drupal.settings.wac[x].class](Drupal.settings.wac[x]);
-            chart.setup();
-            chart.render();
-            element.className += ' wac-chart-processed';
-          }
+  if (Drupal.ahahError) {
+    // Drupal 6
+    Drupal.behaviors.wac = init; 
+  }
+  else {
+    // Drupal 7+
+    Drupal.behaviors.wac = {
+      attach: init
+    };
+  }
+  function init() {
+    if (Drupal.settings.wac) {
+      var x, element, chart;
+      for (var x in Drupal.settings.wac) {
+          element = document.getElementById(Drupal.settings.wac[x].id);
+        if ((' ' + element.className + ' ').indexOf(' ' + 'wac-chart-processed' + ' ') < 0) {
+          chart = new Drupal.Wac[Drupal.settings.wac[x].class](Drupal.settings.wac[x]);
+          chart.setup();
+          chart.render();
+          element.className += ' wac-chart-processed';
         }
       }
-    },
-    detach: function() {
-
     }
-  };
+  }
 })(Class, Drupal);
